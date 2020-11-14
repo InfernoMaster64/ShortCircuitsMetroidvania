3941fbad3bc59eb6cc40c4386a785e3dabf49058
middleNameChaseD <ccdillon@my.waketech.edu>
2020-11-14 07:03:34 -0500

    Move and jump, plus temporary game field
    A and leftArrow, D and rightArrow, space to jump.... Forgot to commit whoops

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    bool onGround;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        onGround = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - .1f, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + .1f, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            rigid.velocity = Vector2.up * 6;
+            onGround = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name.Contains("ground"))
+        {
+            onGround = true;
+        }
+    }
+}
