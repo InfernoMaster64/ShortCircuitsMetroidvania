7d5d96656977bfe46f59d43fd30d8525576ebc92
middleNameChaseD <ccdillon@my.waketech.edu>
2020-11-16 11:57:09 -0500

    Handful of SFX, player moved to castle, midground is now kinematic, jump/move values changed

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,27 +6,31 @@ public class PlayerScript : MonoBehaviour
 {
     Rigidbody2D rigid;
     bool onGround;
-
+    float moveSpeed;
+    float jumpForce;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         onGround = true;
+
+        moveSpeed = .1f;
+        jumpForce = 10;
     }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position = new Vector3(transform.position.x - .1f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x - moveSpeed, transform.position.y, transform.position.z);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position = new Vector3(transform.position.x + .1f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x + moveSpeed, transform.position.y, transform.position.z);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && onGround)
         {
-            rigid.velocity = Vector2.up * 6;
+            rigid.velocity = Vector2.up * jumpForce;
             onGround = false;
         }
     }
